266d9cca870d5babe309b23a931a0a1ce4a3394e
nrjsharma <nrjsharma083@gmail.com>
2019-03-15 10:09:11 +0530

    added progress bar

--- static/js/custom_js/base.js
+++ static/js/custom_js/base.js
@@ -6,11 +6,29 @@ function openFileChooser() {
 
 function startUploading() {
     $("#getFile").change(function () {
+        var progress_bar = new ldBar("#progressBar") ;
         var data = new FormData($('form')[1]);
         data.append('csrfmiddlewaretoken', $.cookie("csrftoken"));
         var form_url = $('#form_upload').attr('action');
         var form_type = $('#form_upload').attr('method');
+        $('.ldBar-label').hide(); // hiding progress_bar percentage
+        $('#form_upload').hide();
         $.ajax({
+            // xhr method is for Progress bar
+            xhr: function () {
+                var xhr = new window.XMLHttpRequest();
+
+                xhr.upload.addEventListener("progress", function (evt) {
+                    var percent = Math.round(evt.loaded / evt.total * 100)
+                    if(percent<100){
+                        progress_bar.set( Math.round(percent));
+                    }
+                    else {
+                        document.getElementById("progressBar").innerHTML="<i>loading...</i>";
+                    }
+                });
+                return xhr;
+            },
             url: form_url,
             type: form_type,
             data: data,
@@ -18,7 +36,12 @@ function startUploading() {
             processData: false,
             contentType: false,
             success: function (data) {
-                alert(data);
+                $.dialog({
+                    backgroundDismiss: true,
+                    animation: 'top',
+                    title: 'Done!',
+                    content: data,
+                });
             },error:function (rs,e) {
                 alert(rs.responseText);
             }
@@ -30,12 +53,3 @@ $(document).ready(function () {
     openFileChooser();
     startUploading();
 });
-
-
-/*
- $.dialog({
- backgroundDismiss: true,
- animation: 'top',
- title: 'Alert!',
- content: 'Simple alert!',
- });*/
