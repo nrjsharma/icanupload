d4093effd9879cb7a40c49bdace887b3c903031b
nrjsharma <nrjsharma083@gmail.com>
2019-06-23 22:20:37 +0530

    update settings prod.py and base.py files

--- settings/base.py
+++ settings/base.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 # for JS and CSS
 STATIC_URL = '/static/'
-STATICFILES_DIRS=os.path.join(BASE_DIR, 'static/'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # For file uploading
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload_media')
-
--- settings/prod.py
+++ settings/prod.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from .base import *  # NOQA
-from db.prod_db import DATABASES
+from settings.base import *  # NOQA
+from settings.db.prod_db import DATABASES
 
 ENV="PRODUCTION"
